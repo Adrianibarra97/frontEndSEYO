af0d9c7a14f3e3d163b1be9460c98944f6881277
Adrianibarra97 <adrian__ibarra@hotmail.com>
2022-05-13 02:41:42 -0300

    Footer terminado!

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import '../css/footer.css';
 
@@ -6,7 +7,18 @@ export class Footer extends Component {
     render() {
         return (
             <div className="footer-container">
-                Footer
+                <p className="footer-p name pweb1">Phone</p>
+                <p className="footer-p pweb2">15-2629-5717</p>
+                <p className="footer-p name pweb3">E-mail</p>
+                <p className="footer-p pweb4">seyo.productions@gmail.com</p>
+                <p className="footer-p name pdev1">Website developed by</p>
+                <p className="footer-p pdev2">Adrian Ibarra</p>
+                <ul className="footer-ul">
+                    <Link class="fab fa-facebook-f footer-link btn btn-secondary" to="/production" />
+                    <Link class="fab fa-twitter footer-link btn btn-secondary" to="/production" />
+                    <Link class="fab fa-instagram footer-link btn btn-secondary" to="/production" />
+                    <Link class="fab fa-youtube footer-link btn btn-secondary" to="/production" />
+                </ul>
             </div>
         );
     }
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,43 +4,74 @@ import { Link } from 'react-router-dom';
 import '../css/navBar.css';
 
 export class NavBar extends Component {
+    state = {
+        showStoreNav: '',
+        showStoreX: 'none'
+    }
 
+    activeNavResponsive = () => {
+        this.setState({
+            showStoreNav: 'block',
+            showStoreX: 'block'
+        });
+    }
+
+    desactiveNavResponsive = () => {
+        this.setState({
+            showStoreNav: 'none',
+            showStoreX: 'none'
+        });
+    }
 
     render() {
         return (
-            <nav class="navbar navbar-expand-lg position-fixed navbar-dark bg-transparent">
-                <div class="container-fluid">
-                    <button class="navbar-toggler"
+            <nav className="navbar navbar-expand-lg position-fixed navbar-dark bg-transparent">
+                <div className="container-fluid">
+                    <button className="navbar-toggler button-nav-1"
                         type="button"
                         data-bs-toggle="collapse"
                         data-bs-target="#navbarTogglerDemo03"
                         aria-controls="navbarTogglerDemo03"
                         aria-expanded="false"
                         aria-label="Toggle navigation"
-
+                        onClick={() => this.activeNavResponsive()}
                     >
-                        <span class="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-
-                    <Link class="navbar-brand" to="/">SEYO</Link>
                     
-                    <div class="collapse navbar-collapse"
+                    <div className="collapse navbar-collapse nav-struct"
                          id="navbarTogglerDemo03"
+                         style={{
+                            display: this.state.showStoreNav }
+                        }
                     >
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
+                        <Link className="navbar-brand" to="/">SEYO</Link>
+
+                        <button type="button"
+                            className="close button-nav-2 bg-transparent"
+                            aria-label="Close"
+                            style={{
+                                display: this.state.showStoreX }
+                            }
+                            onClick={() => this.desactiveNavResponsive()}
+                            >
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+
+                        <ul className="navbar-nav nav-struct-ul">
+                            <li className="nav-item">
                                 <Link className="nav-link" to="/">Home</Link>
                             </li>
-                            <li class="nav-item">
+                            <li className="nav-item">
                                 <Link className="nav-link" to="/production">Production</Link>
                             </li>
-                            <li class="nav-item">
+                            <li className="nav-item">
                                 <Link className="nav-link" to="/musicians">Musicians</Link>
                             </li>
-                            <li class="nav-item">
+                            <li className="nav-item">
                                 <Link className="nav-link" to="/clients">Clients</Link>
                             </li>
-                            <li class="nav-item">
+                            <li className="nav-item">
                                 <Link className="nav-link" to="/contact">Contact</Link>
                             </li>
                         </ul>
