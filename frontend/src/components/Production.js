efb842042e0f153276ea82b8983e275cc1607b0b
Adrianibarra97 <adrian__ibarra@hotmail.com>
2022-05-13 05:29:41 -0300

    Primera parte de production.

--- frontend/src/components/Production.js
+++ frontend/src/components/Production.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
+import '../css/production.css';
+
 export class Production extends Component {
     render() {
         return (
             <div className="production-container">
-                Production
+                <div className="production-section divSecP1">
+                    <h2 className="production-section-h2">Titulo</h2>
+                    <p className="production-section-p">Description1</p>
+                </div>
+                <div className="production-section divSecP2">
+                    <h2 className="production-section-h2">Title1</h2>
+                    <p className="production-section-p">Description1</p>
+                </div>
+                <div className="production-section divSecP3">
+                    <h2 className="production-section-h2">Title1</h2>
+                    <p className="production-section-p">Description1</p>
+                </div>
+                <div className="production-section divSecP4">
+                    <h2 className="production-section-h2">Title1</h2>
+                    <p className="production-section-p">Description1</p>
+                </div>
+                <div className="production-section divSecP5">
+                    <h2 className="production-section-h2">Title1</h2>
+                    <p className="production-section-p">Description1</p>
+                </div>
+                <div className="production-section divSecP6">
+                    <h2 className="production-section-h2">Title1</h2>
+                    <p className="production-section-p">Description1</p>
+                </div>
             </div>
         );
     }
